f65b01325898d58db9ceb5cefe9c5142d888b333
fenn-core <mustafa60simsek@gmail.com>
2026-04-28 22:35:06 +0300

    implemented bme280_readout sensor logic

--- bme280_readout/Core/Inc/bme280.h
+++ bme280_readout/Core/Inc/bme280.h
@@ -8,6 +8,18 @@
 #ifndef INC_BME280_H_
 #define INC_BME280_H_
 
-uint8_t bme280_read_chip_id(void);
+HAL_StatusTypeDef bme280_read_register(uint8_t reg, uint8_t *value);
+
+HAL_StatusTypeDef bme280_write_register(uint8_t reg, uint8_t value);
+
+HAL_StatusTypeDef bme280_read_chip_id(uint8_t *value);
+
+void bme280_init(void);
+
+void read_registers(uint8_t start_reg, uint8_t *buffer, uint8_t length);
+
+uint32_t process_raw_data_20bit(const uint8_t *raw_data);
+
+uint16_t process_raw_data_16bit(const uint8_t *raw_data);
 
 #endif /* INC_BME280_H_ */
--- bme280_readout/Core/Src/bme280.c
+++ bme280_readout/Core/Src/bme280.c
@@ -11,15 +11,52 @@
 #define BME280_ADDR (0x76 << 1)
 #define BME280_REG_CHIP_ID 0xD0
 
+// Chip Registers
+#define CTRL_HUM  0xF2
+#define CTRL_MEAS 0xF4
+#define CONFIG 	  0xF5
+
 extern I2C_HandleTypeDef hi2c1;
 
+typedef struct {
 
-uint8_t bme280_read_chip_id(void){
-	uint8_t chip_id = 0;
 
-	if (HAL_I2C_Mem_Read(&hi2c1, BME280_ADDR, BME280_REG_CHIP_ID, 1, &chip_id, 1, HAL_MAX_DELAY) != HAL_OK){
-		return 0xFF; // error
-	}
+};
 
-	return chip_id;
+
+HAL_StatusTypeDef bme280_read_register(uint8_t reg, uint8_t *value){
+	return HAL_I2C_Mem_Read(&hi2c1, BME280_ADDR, reg, 1, value, 1, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef bme280_write_register(uint8_t reg, uint8_t value){
+	return HAL_I2C_Mem_Write(&hi2c1, BME280_ADDR, reg, 1, &value, 1, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef bme280_read_chip_id(uint8_t *value){
+	return bme280_read_register(BME280_REG_CHIP_ID, value);
+}
+
+void bme280_init(void){
+	bme280_write_register(CTRL_HUM, 0x01);  // humidity oversampling x1
+	bme280_write_register(CTRL_MEAS, 0x27); // temp x1, pressure x1, normal mode
+	bme280_write_register(CONFIG, 0x00);    // filter off, short standby/default-ish
+}
+
+void read_registers(uint8_t start_reg, uint8_t *buffer, uint8_t length){
+	HAL_I2C_Mem_Read(&hi2c1, BME280_ADDR, start_reg, 1, buffer, length, HAL_MAX_DELAY);
+}
+
+uint32_t process_raw_data_20bit(const uint8_t *raw_data){
+	uint32_t msb  =  raw_data[0];
+	uint32_t lsb  =  raw_data[1];
+	uint32_t xlsb = (raw_data[2] >> 4);
+
+	return ((msb << 12)| (lsb << 4) | xlsb);
+}
+
+uint16_t process_raw_data_16bit(const uint8_t *raw_data){
+	uint16_t msb = raw_data[0];
+	uint16_t lsb = raw_data[1];
+
+	return ((msb << 8)| lsb);
 }
